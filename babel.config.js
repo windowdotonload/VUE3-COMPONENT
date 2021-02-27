cac26309e5f76e17a4062835fe59b673e6557b0b
windowdotonload <275981471@qq.com>
2021-02-27 14:41:18 +0800

    mod

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: windowdotonload
+ */
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  plugins: ["@vue/babel-plugin-jsx"]
 }
